75e535e8214d3fae900e291f8ca91792bc0146eb
Nico Weber <nicolasweber@gmx.de>
2012-11-17 13:42:52 -0800

    move prec::Level from c to go

--- include/gong/Parse/Parser.h
+++ include/gong/Parse/Parser.h
@@ -44,27 +44,17 @@ public:
   virtual void print(raw_ostream &OS) const;
 };
 
-/// PrecedenceLevels - These are precedences for the binary/ternary
-/// operators in the C99 grammar.  These have been named to relate
-/// with the C99 grammar productions.  Low precedences numbers bind
-/// more weakly than high numbers.
+/// These are precedences for the binary operators in the Go grammar.
+/// Low precedences numbers bind more weakly than high numbers.
+/// http://golang.org/ref/spec#Operator_precedence
 namespace prec {
   enum Level {
     Unknown         = 0,    // Not binary operator.
-    Comma           = 1,    // ,
-    Assignment      = 2,    // =, *=, /=, %=, +=, -=, <<=, >>=, &=, ^=, |=
-    Conditional     = 3,    // ?
-    LogicalOr       = 4,    // ||
-    LogicalAnd      = 5,    // &&
-    InclusiveOr     = 6,    // |
-    ExclusiveOr     = 7,    // ^
-    And             = 8,    // &
-    Equality        = 9,    // ==, !=
-    Relational      = 10,   //  >=, <=, >, <
-    Shift           = 11,   // <<, >>
-    Additive        = 12,   // -, +
-    Multiplicative  = 13,   // *, /, %
-    PointerToMember = 14    // .*, ->*
+    LogicalOr       = 1,    // ||
+    LogicalAnd      = 2,    // &&
+    Equality        = 3,    // ==, !=, <, <=, >, >=
+    Additive        = 4,    // +, -, |, ^
+    Multiplicative  = 5     // *, /, %, <<, >>, &, , &^
   };
 }
 
