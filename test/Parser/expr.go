fe809f1bb0a42152dd976b0471dc0b25f884617c
Nico Weber <nicolasweber@gmx.de>
2013-01-01 12:10:24 -0800

    fix reference url to refer to the right thing

--- test/Parser/expr.go
+++ test/Parser/expr.go
@@ -215,6 +215,6 @@ func f() {
 
   // You'd think that this is a call of a conversion of a function literal.
   // However, the |(func...| goes down the Result route, which is possibly ok?
-  // See https://code.google.com/p/go/issues/detail?id=4470
+  // See https://code.google.com/p/go/issues/detail?id=4478
   //func()(func(){})()
 }
