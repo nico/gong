19ba4fe915ebedc30667e08180c0d50b587a5443
Nico Weber <nicolasweber@gmx.de>
2013-02-04 19:33:05 -0800

    add -fdiagnostic-parseable-fixit test to fixit-while.go test

--- test/Parser/fixit-while.go
+++ test/Parser/fixit-while.go
@@ -1,17 +1,21 @@
 // RUN: %gong_cc1 -verify %s
+// RUN: %gong_cc1 -fdiagnostics-parseable-fixits %s 2>&1 | FileCheck %s
 
 package p
 
 func f() {
   i := 0
+  // CHECK: fix-it:"{{.*}}":{[[@LINE+1]]:3-[[@LINE+1]]:8}:"for"
   while i < 2 {  // expected-diag {{'while' is spelled 'for' in Go}}
     i++
   }
 
+  // CHECK: fix-it:"{{.*}}":{[[@LINE+1]]:3-[[@LINE+1]]:8}:"for"
   while {  // expected-diag {{'while' is spelled 'for' in Go}}
     break
   }
 
+  // CHECK: fix-it:"{{.*}}":{[[@LINE+2]]:3-[[@LINE+2]]:8}:"for"
   // This one probably won't happen in practice.
   while ;; {  // expected-diag {{'while' is spelled 'for' in Go}}
     break
